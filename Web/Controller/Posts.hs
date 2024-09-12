8a97713a44cfb5ff759e290624f789691c1ed555
frankvp <90110502+frankvp11@users.noreply.github.com>
2024-09-12 19:32:08 +0000

    finish the tutorial

--- Web/Controller/Posts.hs
+++ Web/Controller/Posts.hs
@@ -5,6 +5,8 @@ import Web.View.Posts.Index
 import Web.View.Posts.New
 import Web.View.Posts.Edit
 import Web.View.Posts.Show
+import qualified Text.MMark as MMark
+import Control.Lens (none, non)
 
 instance Controller PostsController where
     action PostsAction = do
@@ -19,6 +21,9 @@ instance Controller PostsController where
 
     action ShowPostAction { postId } = do
         post <- fetch postId
+            >>= pure . modify #comments (orderByDesc #createdAt)
+            >>= fetchRelated #comments
+
         render ShowView { .. }
 
     action EditPostAction { postId } = do
@@ -56,4 +61,11 @@ instance Controller PostsController where
 buildPost post = post
     |> fill @'["title", "body"]
     |> validateField #title nonEmpty
-    
+    |> validateField #body nonEmpty
+    |> validateField #body isMarkdown
+
+isMarkdown :: Text -> ValidatorResult
+isMarkdown text =
+    case MMark.parse "" text of
+        Left _ -> Failure "Please provide valid Markdown"
+        Right _ -> Success
